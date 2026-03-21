b5a50aac9323a31f41718843a4164f66d9950dd4
RanasingheDD <dishand398@gmail.com>
2026-03-21 12:39:12 +0530

    add vercel backend api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://sparegrid-4k4f.vercel.app',
   headers: { 'Content-Type': 'application/json' },
 })
 
