834e1856d733c5f4b4019d6e189e1b290f2fee07
RanasingheDD <dishand398@gmail.com>
2026-03-21 12:49:27 +0530

    added frontend url to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="SpareGrid API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://sparegrid.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
