1c63abac9d272a95f3e6290351d02879f05f78d0
RanasingheDD <dishand398@gmail.com>
2026-03-31 11:37:39 +0530

    order alert mail

--- backend/app/services/email_templates.py
+++ backend/app/services/email_templates.py
@@ -5,7 +5,7 @@ from app.policies import MARKETPLACE_POLICIES, calculate_order_total
 
 
 SUPPORT_EMAIL = "support.lankaparts@gmail.com"
-ORDER_ALERT_EMAIL = "support.lankaparts@gmail.com"
+ORDER_ALERT_EMAIL = "dishand398@gmail.com"
 SITE_URL = "https://lankaparts.live"
 
 
